5ecf95ffd136c243e3fbdb89e7b9c0bbac73d751
Manuel <manuel@unterriker.biz>
2025-09-20 13:39:05 +0000

    run

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ set -e
 source .env
 
 LOCAL_ONLY=True
-SECRET_KEY=4bd5dcfc32602307e5101efcfedbb7c3e770a8167d596933
-UPLOADS_DIR=/home/manuel/file-wiz/uploads
-PROCESSED_DIR=/home/manuel/file-wiz/processed
+SECRET_KEY=
+UPLOADS_DIR=./uploads
+PROCESSED_DIR=./processed
 # Start Gunicorn in the background
 gunicorn -w 4 --threads 2 -k uvicorn.workers.UvicornWorker --forwarded-allow-ips='*' --error-logfile - --access-logfile - main:app -b 0.0.0.0:8000 &
 echo "Started Gunicorn..."
